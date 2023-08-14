c080fdeb26b97ec6dc07a29b35852cfe52b33bec
chisato5582 <your-amac5582@gmail.com>
2023-08-14 18:00:02 +0900

    背景画像追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,13 @@ module.exports = {
     extend: {
       colors: {
         "button-color":"#B77878",
+        "hover-color":"#B46161",
+        "button-font":"#F9EFEF",
         "footer-color":"#897670",
         "text-color":"6c6363",
         "header-color":"#87AE8D",
-        "flash-color":"#EDEBDD"
+        "flash-color":"#EDEBDD",
+        "font-color":"#9E616A"
       }
     },
   },
