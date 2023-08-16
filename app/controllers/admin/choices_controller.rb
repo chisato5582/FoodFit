564b2cdae8474b4d4683580d040b9137b640d184
chisato5582 <your-amac5582@gmail.com>
2023-08-16 22:48:12 +0900

    管理者機能の追加

---
+++ app/controllers/admin/choices_controller.rb
@@ -0,0 +1,30 @@
+class Admin::ChoicesController < Admin::BaseController
+
+    def new
+        @choice = Choice.new
+    end
+
+    def create; end
+
+    def edit; end
+
+    def update
+        if @choice.update(choice_params)
+            redirect_to admin_choice_path, success: "問題選択肢を更新しました"
+        else
+            flash.now[:danger] = "選択肢の変更に失敗しました"
+            render :edit
+        end
+    end
+
+    def destroy
+        @choice.destroy!
+        redirect_to admin_choices_path, success: "選択肢を削除しました"
+    end
+
+    private
+
+    def choice_params
+        params.require(:choice).permit(:choice, :correct)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,9 @@ Rails.application.routes.draw do
     get 'login', to: 'user_sessions#new'
     post 'login', to: 'user_sessions#create'
     delete 'logout', to: 'user_sessions#destroy'
+
+    resources :questions
+    resources :choices, only: %i[new create edit update destroy]
   end
 
 end
